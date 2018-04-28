0206136394ee5732c7c6227d63fb9228abd38e58
Mikhail S. Pobolovets <styx.mp@gmail.com>
2018-04-28 14:21:31 +0300

    Add no-compose-overuse rule

---
+++ tests/lib/rules/no-compose-overuse.js
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview Forbid R.compose when it is called at the same place
+ * @author no-compose-overuse
+ */
+
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-compose-overuse');
+const {RuleTester} = require('eslint');
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+});
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+ruleTester.run('no-compose-overuse', rule, {
+
+  valid: [
+    {code: "R.compose(createAction, R.prop('FETCH_ALL_WITH_CONTENTS_REQUEST'))"},
+    {code: 'R.compose(createAction, someFunc1, someFunc2)'},
+  ],
+
+  invalid: [
+    {
+      code: "R.compose(createAction, R.prop('FETCH_ALL_WITH_CONTENTS_REQUEST'))(types)",
+      output: "createAction(R.prop('FETCH_ALL_WITH_CONTENTS_REQUEST', types))",
+      errors: [{
+        message: "Don't use R.compose when it is called at the same place",
+        type: 'CallExpression',
+      }],
+    },
+    {
+      code: 'R.compose(createAction, someFunc1, someFunc2)(types)',
+      output: 'createAction(someFunc1(someFunc2(types)))',
+      errors: [{
+        message: "Don't use R.compose when it is called at the same place",
+        type: 'CallExpression',
+      }],
+    },
+  ],
+});
